5061a05f338daa6fe5692d74606bb3a93f349084
Mark G <markghaida@gmail.com>
2020-10-01 02:30:15 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,7 +9,7 @@ end
 # code your input_to_index and move method here!
 
 def input_to_index (user_input)
-   integer = "#{user_input}".to_i
+   index = "#{user_input}".to_i
    index_number = integer - 1 
 end
 
